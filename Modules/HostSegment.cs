2dce3065ec11d5886c2b5e598739b594ff74a9bb
Lucas Pimentel <lucaspimentel@gmail.com>
2024-07-02 19:48:02 -0400

    add HostSegment (unused)

---
+++ Modules/HostSegment.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Text;
+
+namespace Prompt.Modules;
+
+internal readonly struct HostSegment : ISegment
+{
+    private const string Prefix = "   ";
+
+    private readonly string _hostname = Dns.GetHostName();
+
+    public HostSegment()
+    {
+    }
+
+    public int UnformattedLength  => _hostname.Length + Prefix.Length;
+
+    public void Append(ref ValueStringBuilder sb)
+    {
+        sb.Append("[blue]");
+        sb.Append(Prefix);
+        sb.Append(_hostname);
+        sb.Append(" [/]");
+    }
+
+    public override string ToString()
+    {
+        return _hostname;
+    }
+}
